1cbe04cf65c191ebb114db68320be8820632aed5
Megan Gedde <hoglu070@d.umn.edu>
2022-05-13 20:09:53 -0500

    added zookeeper pages

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const PORT = process.env.PORT || 3003;
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const PORT = process.env.PORT || 3002;
 const app = express();
   // parse incoming string or array data
   app.use(express.urlencoded({ extended: true }));
@@ -7,12 +9,12 @@ const app = express();
   app.use(express.json());
   app.use(express.static('public'));
   app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+  app.use('/', htmlRoutes);
+  
 const { animals } = require('./data/animals');
 const fs = require('fs');
 const path = require('path');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+
 
 
 app.listen(PORT, () => {
